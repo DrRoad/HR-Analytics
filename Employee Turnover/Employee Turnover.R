6869ed16dc5049de72d95e1d9442bf488a188763
Mochabits <teresating886@users.noreply.github.com>
2017-09-19 12:20:20 -0600

    Employee Turnover

---
+++ Employee Turnover/Employee Turnover.R
@@ -0,0 +1,46 @@
+library("QuantPsyc")
+library(ggplot2)
+setwd("/Users/Teresa/Documents/R/Employee Turnover")
+emp <- read.csv("Individual Turnover.csv", header = T, sep=",",
+                stringsAsFactors = F)
+
+
+
+# Create a summary table
+emp.table <- table(emp.data)
+###########################################################
+# Independent samples T-test
+# Independent variable(categorical)- function and LondonorNot
+# dependent variable (continuous) - EMPsurvEngagement
+###########################################################
+
+# independent t-test for LondonorNot vs engegement
+ldn <- empSurvey$LondonorNot == 1
+ldn.eng <- empSurvey[ldn,]$EMPsurvEngagement
+noLdn.eng <- empSurvey[!ldn,]$EMPsurvEngagement
+t.test(ldn.eng, noLdn.eng)
+
+# independent t-test for Function vs engegement
+s <- empSurvey$Function == 1
+sales.eng <- empSurvey[s,]$EMPsurvEngagement
+prof.eng <- empSurvey[!s,]$EMPsurvEngagement
+t.test(sales.eng, prof.eng)
+
+# using multiple regression to predict team-level engagement
+eng.lm <- lm(EMPsurvEngagement ~ Function + GroupSize + PercentMale + BAME +
+             EmpSurvOrgIntegrity + EmpSurvSupervisor, data=empSurvey)
+
+summary(eng.lm)
+
+# standardized beta coefficient - to know which variable has bigger impact
+# The higher the absolute value of the beta coefficient, the stronger the effect.
+lm.beta(eng.lm)
+
+
+
+
+
+
+
+
+
