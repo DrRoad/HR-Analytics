fa2e840957b0c6368931fae1c86017f165ce5f88
Mochabits <teresating886@users.noreply.github.com>
2017-10-12 13:44:25 -0600

    edits on "offers made"

--- Recruitment/Applicants.R
+++ Recruitment/Applicants.R
@@ -1,4 +1,5 @@
 library("plyr")
+library("BaylorEdPsych")
 setwd("/Users/Teresa/Documents/R/Recruitment")
 
 apps <- read.csv("Applicants.csv", header = T, sep=",",
@@ -37,6 +38,10 @@ AcceptNY <- count(apps, 'AcceptNY')
 AcceptNY
 prop.table(table(apps$AcceptNY))
 
+######################################################
+## Gender and BAME with shortlisting
+######################################################
+
 # Chi square to explore gender on shortlisting 
 gen.short.tab <- table(apps$Gender, apps$ShortlistedNY)
 rownames(gen.short.tab)<-c('Male','Female')
@@ -52,7 +57,32 @@ bame.short.tab
 chisq.test(bame.short.tab)
 
 # Combine gender and bame to predict shortlisting
+logit.short <- glm(ShortlistedNY ~ BAMEyn + Gender,
+                   family=binomial(link='logit'),data=apps)
+summary(logit.short)
 
+# Pseudo R-square values
+PseudoR2(logit.short)
+
+# Odds ratio
+exp(logit.short$coefficients)
+
+######################################################
+## Gender and BAME with Offers made
+######################################################
+# Chi square to explore gender with offers made 
+gen.offer.tab <- table(apps$Gender, apps$OfferNY)
+rownames(gen.offer.tab)<-c('Male','Female')
+colnames(gen.offer.tab)<-c('No','Yes')
+gen.offer.tab
+chisq.test(gen.offer.tab)
+
+# Chi square to explore BAME with offers made 
+bame.offer.tab <- table(apps$BAMEyn, apps$OfferNY)
+rownames(bame.offer.tab)<-c('Male','Female')
+colnames(bame.offer.tab)<-c('No','Yes')
+bame.offer.tab
+chisq.test(bame.offer.tab)
 
 
 
